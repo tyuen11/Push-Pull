b5cdc231d627dad3d62b2555a85c3d41e0ea6a5c
tyuen11 <timothy.yuen11@gmail.com>
2017-08-14 17:07:23 -0400

    Code to add User to database using their UID in Test.js

--- WebstormProjects/Summer2017/Test.js
+++ WebstormProjects/Summer2017/Test.js
@@ -28,18 +28,27 @@
 // creating variables for elements from html (Account_Page.html)
     const btnItem = document.getElementById("Add_Items");
     const btnCancel = document.getElementById("Cancel");
+    const btnAdd = document.getElementById("Add");
+    var item_textfield = document.getElementById("item_textfield");
 
-    // Login Event
+    //start path for all users
+    var start = database.child("users");
+    var item = database.child("users/items");
+
+
+// Login Event
     if (btnLogin) {
         btnLogin.addEventListener("click", e => {
             // get email and password
             var email = txtEmail.value;
             var pass = txtPassword.value;
             var auth = firebase.auth();
+            //user's uid
             // sign in
             var promise = auth.signInWithEmailAndPassword(email, pass);
             promise.catch(e => console.log(e.message));
 
+            // Homepage to User's Page after registering
             firebase.auth().onAuthStateChanged(firebaseUser => {
                 if (firebaseUser) {
                     console.log(firebaseUser);
@@ -52,21 +61,29 @@
     }
 
     // Sign Up Event
-    if (btnSignUp) {
-        btnSignUp.addEventListener("click", e => {
-            // TODO: check if email and password is valid
-            var email = txtEmail.value;
-            var pass = txtPassword.value;
-            var auth = firebase.auth();
-            //create
-            var promise = auth.createUserWithEmailAndPassword(email, pass);
-            promise.catch(e => console.log(e.message));
+if (btnSignUp) {
+    btnSignUp.addEventListener("click", e => {
+        // TODO: check if email and password is valid
+        var email = txtEmail.value;
+        var pass = txtPassword.value;
+        var auth = firebase.auth();
+        //create account
+        var promise = auth.createUserWithEmailAndPassword(email, pass);
+        promise.catch(e => console.log(e.message));
+        firebase.auth().onAuthStateChanged(firebaseUser => {
+            if (firebaseUser) {
+                console.log(firebaseUser);
+                window.location.assign("Account_Page.html");
+            } else {
+                console.log("not logged in");
+            }
         });
-    }
 
-    // Logout Event
+    });
+}
+// Logout Event
     if (btnLogout) {
-        btnLogout.addEventListener("click", e => {
+    btnLogout.addEventListener("click", e => {
             firebase.auth().signOut();
             window.location.replace("HomePage.html");
         })
@@ -75,20 +92,26 @@
     firebase.auth().onAuthStateChanged(firebaseUser => {
         if (firebaseUser) {
             console.log(firebaseUser);
+            console.log(firebaseUser.uid);
         } else {
             console.log("not logged in");
     }
 
   // get user's name onto Account_Page.html header
     var user = firebase.auth().currentUser;
+        console.log(user.uid);
     var email;
     if (user){
         email = user.email;
         document.getElementById("UserName").innerHTML = email;
     }
+        start.child(user.uid).set({
+            UID: user.uid,
+            Email: email
+        });
     // all in Account_Page.html
 
-     if (btnItem) {
+     if (btnItem){
         btnItem.addEventListener("click", e => {
            document.getElementById("Add_Item").style.display = "flex";
         })
@@ -99,6 +122,22 @@
 
         })
     }
+    if (btnAdd){
+        btnAdd.addEventListener("click", e=> {
+            console.log(item_textfield.value);
+            adding_to_list(item_textfield.value);
+        })
+    }
+
+    // create node for each user that only they can read and write to
+    function adding_to_list (item_textfield){
+            database.update(item.update({
+                item: item_textfield
+            }));
+
+
+        }
+
 
 
 });
