afe3658c92423768b5229a4928c00228679fe6b6
tyuen11 <timothy.yuen11@gmail.com>
2017-08-29 22:39:39 -0400

    Code to read data from database and log into console

--- WebstormProjects/Summer2017/Test.js
+++ WebstormProjects/Summer2017/Test.js
@@ -7,7 +7,7 @@
 
 
     // Initialize Firebase
-    const config = {
+const config = {
         apiKey: "AIzaSyBwuBhMX_ydPOg31SuZ5izd90NbwcTKyHA",
         authDomain: "user-push-pull.firebaseapp.com",
         databaseURL: "https://user-push-pull.firebaseio.com",
@@ -15,30 +15,32 @@
         storageBucket: "user-push-pull.appspot.com",
         messagingSenderId: "1058245797988"
     };
-    firebase.initializeApp(config);
-    var database  = firebase.database().ref();
+firebase.initializeApp(config);
+var database  = firebase.database().ref();
+
 
 // creating variables for elements from html (HomePage.html)
-    const txtEmail = document.getElementById("email");
-    const txtPassword = document.getElementById("password");
-    const btnLogin = document.getElementById("Login_Button");
-    const btnSignUp = document.getElementById("Register_Button");
-    const btnLogout = document.getElementById("Logout");
+const txtEmail = document.getElementById("email");
+const txtPassword = document.getElementById("password");
+const btnLogin = document.getElementById("Login_Button");
+const btnSignUp = document.getElementById("Register_Button");
+const btnLogout = document.getElementById("Logout");
 
 // creating variables for elements from html (Account_Page.html)
-    const btnItem = document.getElementById("Add_Items");
-    const btnCancel = document.getElementById("Cancel");
-    const btnAdd = document.getElementById("Add");
-    var item_textfield = document.getElementById("item_textfield");
+const btnItem = document.getElementById("Add_Items");
+const btnCancel = document.getElementById("Cancel");
+const btnAdd = document.getElementById("Add");
+const item_textfield = document.getElementById("item_textfield");
+const item_displayer = document.getElementById("item_list");
 
-    //start path for all users
-    var start = database.child("users");
+//start path for all users
+var start = database.child("users");
 
 
 // Login Event
-    if (btnLogin) {
-        btnLogin.addEventListener("click", e => {
-            // get email and password
+if (btnLogin) {
+    btnLogin.addEventListener("click", e => {
+        // get email and password
             var email = txtEmail.value;
             var pass = txtPassword.value;
             var auth = firebase.auth();
@@ -56,8 +58,8 @@
                     console.log("not logged in");
                 }
             });
-        });
-    }
+    });
+}
 
 // Sign Up Event
 if (btnSignUp) {
@@ -81,17 +83,17 @@ if (btnSignUp) {
                 console.log("not logged in");
             }
         });
-
     });
 }
+
 // Logout Event
 if (btnLogout) {
     btnLogout.addEventListener("click", e => {
             firebase.auth().signOut();
             window.location.replace("HomePage.html");
-        })
-    }
-
+    })
+} //END
+//---------------------------------------------------------------------------------------------
 firebase.auth().onAuthStateChanged(firebaseUser => {
         if (firebaseUser) {
             console.log(firebaseUser);
@@ -99,20 +101,17 @@ firebase.auth().onAuthStateChanged(firebaseUser => {
             console.log("not logged in");
 
         }
-
-
   // get user's name onto Account_Page.html header
     var user = firebase.auth().currentUser;
-        console.log(user.uid);
+    console.log(user.uid);
     var email;
     if (user){
         email = user.email;
         document.getElementById("UserName").innerHTML = email;
-        // TODO: show all user's ITEMS
+    } //END
+    var database_to_show = firebase.database().ref("users");
 
-
-    }
-        start.child(user.uid).update({
+    start.child(user.uid).update({
             UID: user.uid,
             Email: email,
             });
@@ -134,31 +133,68 @@ firebase.auth().onAuthStateChanged(firebaseUser => {
             adding_to_list(item_textfield.value);
         })
     }
-
     // adds item to user's node
     function adding_to_list (item_textfield){
         //directs to user uid node in database
-        var user_path = start.child(user.uid).child("item");
+        var user_path = start.child(user.uid).child("items");
         user_path.push({
                 item: item_textfield
             });
         console.log("Added");
         //TODO: ^^ make visible to user
-    }
-    console.log("...");
+    } //END
+
+    //get items
     show();
-
-
-    var database_to_show = firebase.database().ref(user.uid);
     function show() {
         database_to_show.on("child_added", function (snapshot, prevChildKey) {
+
+           /*
             var newItem = snapshot.val();
+            var item_value = newItem.items;
             console.log(newItem);
-            console.log(prevChildKey.val());
+            console.log(item_value);
+            console.log(snapshot.key); // gets user uid
+            //console.log(snapshot.child("item").val()); //same as line 154
+            //*/
+            //TODO: create a path that goes from user -> uid -> item
+            //NOT REALLY SECURITY RULES (TEMPORARY);
+            // https://stackoverflow.com/questions/21500946/firebase-how-to-list-user-specific-data
+            //https://www.firebase.com/docs/web/guide/structuring-data.html
+            //firebase pathing https://www.npmjs.com/package/firebase-path
+
+            if (user.uid === snapshot.key) {
+                var newItem = snapshot.val();
+                console.log("this shows the user");
+                console.log(newItem);
+                console.log("this shows the items node");
+                console.log(newItem.items);
+
+
+                //console.log(snapshot.child("item").val()); //same as line 154
+                //TODO: create a path that goes from user -> uid -> item
+
+
+                //displaying items on Account_Page.html
+                var node = document.createElement("p");
+                var textnode = document.createTextNode(newItem.items);
+                node.appendChild(textnode);
+                item_displayer.appendChild(node);
+
+            }
+
+
+
+           /*
+           var node = document.createElement("p");
+            var textnode = document.createTextNode(newItem.item);
+            node.appendChild(textnode);
+            item_displayer.appendChild(node);
+            //*/
         });
     }
-});
 
+});
 
 
 
